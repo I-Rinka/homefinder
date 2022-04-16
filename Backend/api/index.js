47629d83926868a67e67d72e8d2bce3aa3907753
I-Rinka <I_Rinka@outlook.com>
2022-04-16 10:01:07 +0800

    backend query OK

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,6 +1,7 @@
 import Express from "express";
-import { selecthouse } from "./selecthouse.js";
+import { selectHouse, selectRecord } from "./sql.js";
 
 export const api = Express.Router();
 
-api.get('/selecthouse', selecthouse);
+api.get('/selecthouse', selectHouse);
+api.get('/selectrecord', selectRecord);
--- Backend/api/sql.js
+++ Backend/api/sql.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 import { url } from "../db/mongo.js";
 
-export function selecthouse(req, res) {
+export function selectHouse(req, res) {
     let lat1 = req.query.lat1;
     let lat2 = req.query.lat2;
 
@@ -16,10 +16,10 @@ export function selecthouse(req, res) {
 
     let error = 1;
     if (lat1 && lat2 && lng1 && lng2) {
-        lat1=parseFloat(lat1);
-        lat2=parseFloat(lat2);
-        lng1=parseFloat(lng1);
-        lng2=parseFloat(lng2);
+        lat1 = parseFloat(lat1);
+        lat2 = parseFloat(lat2);
+        lng1 = parseFloat(lng1);
+        lng2 = parseFloat(lng2);
         MongoClient.connect(url, (err, db) => {
             if (err) {
                 error = 2;
@@ -28,6 +28,36 @@ export function selecthouse(req, res) {
             let dbo = db.db("homefinder");
             let query = { lat: { $lte: lat1, $gte: lat2 }, lng: { $gte: lng1, $lte: lng2 } };
             dbo.collection("house_coord").find(query).toArray((err, result) => {
+                if (err) {
+                    error = 3;
+                    console.log(err);
+                } else {
+                    console.log("res:", result);
+                    error = 0;
+                    res.send(result)
+                }
+                db.close();
+            });
+        });
+    }
+    else {
+        res.send(error);
+    }
+}
+
+export function selectRecord(req, res) {
+    let houseName = req.query.houseName;
+    console.log(houseName);
+    let error = 1;
+    if (houseName) {
+        MongoClient.connect(url, (err, db) => {
+            if (err) {
+                error = 2;
+                res.send(error);
+            }
+            let dbo = db.db("homefinder");
+            let query = { block: houseName };
+            dbo.collection("sales_records").find(query).toArray((err, result) => {
                 if (err) {
                     error = 3;
                     console.log(err);
