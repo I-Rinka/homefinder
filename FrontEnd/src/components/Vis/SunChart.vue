054fcc1e8d64d109042d7b224810ca756e25d537
I-Rinka <I_Rinka@outlook.com>
2022-04-21 11:47:21 +0800

    rotation OK

--- FrontEnd/src/components/Vis/SunChart.vue
+++ FrontEnd/src/components/Vis/SunChart.vue
@@ -2,9 +2,11 @@
     <div>
 
         <svg viewBox="-50 -50 100 100" width="80" height="80" xmlns="http://www.w3.org/2000/svg">
-            <circle class="arc" cx="0" cy="0" r="30" fill="none" :stroke="color_array[1]" stroke-width="15"
-                stroke-linecap="round" stroke-dasharray="188.5"
-                :style="{ strokeDashoffset: arclen, transform: 'rotate(-90deg)' }" />
+            <circle class="arc" cx="0" cy="0" fill="none" :stroke="color_array[1]" stroke-width="15"
+                stroke-linecap="round" :style="{
+                    r: radius, strokeDashoffset: `${GetDash(end, radius)}`, strokeDasharray: `${GetDash(0, radius)}`,
+                    transform: `rotate(${GetRotation(end, 45)}deg)`
+                }" />
             <!-- <circle cx="50%" cy="50%" r="100" fill="none" stroke="blue" stroke-width="15" stroke-dashoffset="200" -->
             <!-- stroke-dasharray="300" transform="rotate(-90 60 60)" stroke-linecap="round" /> -->
         </svg>
@@ -19,21 +21,37 @@ import * as d3 from "d3";
 
 let color_array = d3.schemeSet1;
 
+let end = 280;
+let radius = 20;
 
-const data = reactive({
-    angle: 270
-})
-if (data.angle < 0) data.angle = 0;
-if (data.angle > 360) data.angle = 360;
-const arclen = GetDash(data.angle, 30);
-function GetDash(angle: number, stroke_width: number): number {
-    return Math.PI * stroke_width * (360 - angle) / 180.0
+function GetDash(angle: number, radius: number): number {
+    return Math.PI * radius * (360 - angle) / 180.0
 }
 
-console.log(GetDash(0, 30))
+function GetRotation(end: number, orientation: number): number {
+    return -orientation - end / 2
+}
 
-/* Convert to path length using formula r * θ (radians) */
-/* Set stroke-dashoffset attribute to new arc length */
+function GetOrientation(my_coordinates: [number, number], target_coordinates: [number, number]): number {
+    let normalized_coord = [target_coordinates[0] - my_coordinates[0], target_coordinates[1] - my_coordinates[1]]
+    // first quartile
+    if (normalized_coord[0] >= 0 && normalized_coord[1] >= 0) {
+        return Math.atan2(normalized_coord[1], normalized_coord[0]) * 180 / Math.PI;
+    }
+    // second quartile
+    else if (normalized_coord[0] < 0 && normalized_coord[1] >= 0) {
+        return 180 - (Math.atan2(normalized_coord[1], -normalized_coord[0]) * 180 / Math.PI);
+    }
+    // third quartile
+    else if (normalized_coord[0] < 0 && normalized_coord[1] < 0) {
+        return 180 + (Math.atan2(-normalized_coord[1], -normalized_coord[0]) * 180 / Math.PI);
+    }
+    // forth quartile
+    else if (normalized_coord[0] >= 0 && normalized_coord[1] < 0) {
+        return 360 - (Math.atan2(-normalized_coord[1], normalized_coord[0]) * 180 / Math.PI);
+    }
+    return 0;
+}
 
 </script>
 
