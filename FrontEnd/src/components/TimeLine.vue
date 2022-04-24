ca13815a44dbd9deabd8d9928aa33e77e7c05ce1
I-Rinka <I_Rinka@outlook.com>
2022-04-24 18:28:43 +0800

    componenize the button

--- FrontEnd/src/components/TimeLine.vue
+++ FrontEnd/src/components/TimeLine.vue
@@ -1,25 +1,20 @@
 <template>
     <div class="timeline">
         <div class="runway">
-            <div class="button-frame" :style="{ left: `${xOffset}px` }">
-                <div class="button">
-                </div>
-            </div>
+            <slider-button></slider-button>
         </div>
     </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from "@vue/runtime-core";
+import  SliderButton from "./TimeLine/SliderButton.vue";
 
-let xOffset = ref(0)
 
 let pressed = false;
 
-// pressed => move (element left) => 
-
 onMounted(() => {
-    let runway = document.getElementsByClassName("timeline");
+    let runway = document.getElementsByClassName("runway");
     for (let i = 0; i < runway.length; i++) {
         const element = runway[i];
 
@@ -28,12 +23,6 @@ onMounted(() => {
                 xOffset.value = e.clientX - 12
             }
         })
-    }
-
-    let button = document.getElementsByClassName("timeline");
-    for (let i = 0; i < button.length; i++) {
-        const element = button[i];
-
         element.addEventListener('pointerdown', (e) => {
             pressed = true;
         })
@@ -45,7 +34,9 @@ onMounted(() => {
             pressed = false;
         })
     }
+
 })
+
 </script>
 
 <style lang="less" scoped>
@@ -63,21 +54,5 @@ onMounted(() => {
     top: 40%;
     width: 100%;
     background-color: ghostwhite;
-}
-
-.button-frame {
-    filter: drop-shadow(0.5px 0.5px 2px rgba(0, 0, 0, 0.5));
-    cursor: -webkit-grabbing;
-    position: inherit;
-    top: -25%;
-    height: 125%;
-    width: 12px;
-}
-
-.button {
-    height: 100%;
-    width: 100%;
-    clip-path: polygon(0 0, 100% 0, 70% 30%, 70% 100%, 30% 100%, 30% 30%, 0 0);
-    background-color: rgb(200, 26, 10);
 }
 </style>
---
+++ FrontEnd/src/components/TimeLine/SliderButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="button-frame" :style="{ left: `${xOffset}px` }">
+        <div class="button">
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity";
+
+let xOffset = ref(0);
+
+</script>
+
+<style>
+.button-frame {
+    filter: drop-shadow(0.5px 0.5px 2px rgba(0, 0, 0, 0.5));
+    cursor: -webkit-grabbing;
+    position: inherit;
+    top: -25%;
+    height: 125%;
+    width: 12px;
+}
+
+.button {
+    height: 100%;
+    width: 100%;
+    clip-path: polygon(0 0, 100% 0, 70% 30%, 70% 100%, 30% 100%, 30% 30%, 0 0);
+    background-color: rgb(200, 26, 10);
+}
+</style>
