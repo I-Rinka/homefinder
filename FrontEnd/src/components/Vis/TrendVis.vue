74fd7961893fb928d90303dcabe45c23da324e56
I-Rinka <I_Rinka@outlook.com>
2022-05-03 13:40:20 +0800

    initial trend OK

--- FrontEnd/src/components/Vis/TrendVis.vue
+++ FrontEnd/src/components/Vis/TrendVis.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="trend-vis">
-    <el-button style="pointer-events: all" @click="PrintRecords"
-      >Show Records</el-button
-    >
-    <div v-for="t in records" :key="t">
-      {{ t.time }}
-    </div>
+    <svg
+      ref="visMountPoint"
+      width="600"
+      height="600"
+      xmlns="http://www.w3.org/2000/svg"
+    ></svg>
   </div>
 </template>
 
 <script setup>
-import { computed, onMounted, onUpdated, toRaw } from "@vue/runtime-core";
+import { computed, onMounted, onUpdated, toRaw, ref } from "@vue/runtime-core";
+import * as d3 from "d3";
 
 const props = defineProps({
   history_records: {
@@ -28,9 +29,7 @@ const props = defineProps({
 const records = computed(() => {
   // select All
   if (props.selection_time === null || props.selection_time[0].year === 0) {
-    return props.history_records.map((d) => {
-      return { time: new Date(d.time).toUTCString(), price: d.price };
-    });
+    return props.history_records;
   } else {
     let l = Date.UTC(
       props.selection_time[0].year,
@@ -43,7 +42,7 @@ const records = computed(() => {
     return props.history_records
       .filter((d) => d.time >= l && d.time <= r)
       .map((d) => {
-        return { time: new Date(d.time).toUTCString(), price: d.price };
+        return { time: d.time, price: d.price };
       });
   }
 });
@@ -52,8 +51,53 @@ function PrintRecords() {
   console.log(records.value);
 }
 
-onMounted(() => {});
-onUpdated(() => {});
+function Update() {
+  let x = d3
+    .scaleTime()
+    .range([20, 200])
+    .domain([
+      d3.min(records.value, (d) => new Date(d.time)),
+      d3.max(records.value, (d) => new Date(d.time)),
+    ]);
+
+  const height = 200;
+  let y = d3
+    .scaleLinear()
+    .range([10, 200])
+    .domain([0, d3.max(records.value, (d) => d.price)]);
+
+  let svg = d3.select(visMountPoint.value);
+
+  let bar = svg.selectAll("rect").data(records.value);
+  console.log(records.value);
+
+  bar
+    .exit()
+    .transition()
+    .attr("transform", (d) => "translate(" + 0 + "," + height + ")")
+    .duration(600)
+    .attr("height", 0)
+    .remove();
+  bar
+    .enter()
+    .append("rect")
+    .style("fill", "steelblue")
+    .attr("width", (200 - 20) / records.value.length)
+    .attr("transform", (d) => "translate(" + x(d.time) + "," + height + ")")
+    .transition()
+    .duration(600)
+    .attr("transform", (d) => "translate(" + x(d.time) + "," + y(d.price) + ")")
+    .attr("height", (d) => height - y(d.price));
+}
+
+let visMountPoint = ref(null);
+onMounted(() => {
+  //   console.log(visMountPoint.value);
+  Update();
+});
+onUpdated(() => {
+  Update();
+});
 </script>
 
 <style>
