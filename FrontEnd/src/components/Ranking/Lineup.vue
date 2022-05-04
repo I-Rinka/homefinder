72f158ea152f9b1c4e1cd68ef6e24d50e34487fd
Llllrf <1269957412@qq.com>
2022-05-05 05:12:41 +0800

    calculate scaled value

--- FrontEnd/src/components/Ranking/Lineup.vue
+++ FrontEnd/src/components/Ranking/Lineup.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="line-up">    
     <div class="weight-strip">
-      <div
-        class="enabled"
+      <div class="enabled"
         v-for="d in enabled_strip"
         :key="d.name"
         :style="{
@@ -68,7 +67,8 @@
 <script setup>
 import { reactive } from "@vue/reactivity";
 import { useStore } from "../store/weight";
-import { computed, onMounted } from "@vue/runtime-core";
+import { computed, onMounted, watch } from "@vue/runtime-core";
+import * as d3 from "d3";
 
 const store = useStore();
 const props = defineProps({
@@ -82,20 +82,16 @@ const props = defineProps({
 // the reactive data
 const data = reactive({
   mapping_dialog_visible: false,
-  ranking_score: [],
+  // ranking_score: [],
 })
 
-const scaled_records = [{a:1, b:2}, {a:2, b:3}, {a:5, b:1}]
-
-// const 
-
-function HandleConfirmMapping() {
-  data.mapping_dialog_visible = false
-}
+const nominal_attr_name = ['direction', 'decoration', 'position', 'type']
+const scale_list = new Map  // the scale of each attr
 
 // Add default criteria, this should be name of records criteria. Like price, area etc.
 // name, color, enabled(default is disabled)
 store.AddCriteria("area", "#a6cee3", true);
+store.AddCriteria("direction", "#ffff99", true);
 store.AddCriteria("decoration", "#1f78b4");
 store.AddCriteria("deal_price", "#b2df8a");
 store.AddCriteria("unit_price", "#33a02c", true);
@@ -113,6 +109,82 @@ const strip_percentage_sum = computed(() => {
   enabled_strip.value.forEach((d) => (sum += d.weight));
   return sum;
 });
+
+watch(
+  () => props.origin_records,   // calculate the scaled value of the default attr list, can't use onMounted because props is slower
+  () => {
+  let default_attr_list = enabled_strip.value.map(s => s.name)
+  default_attr_list.forEach((attr) => {
+    CalculateScale(attr)
+    CalculateScaledRecords(attr)
+  })
+})
+
+watch(
+  () => enabled_strip.value,
+  (new_val, old_val) => {
+    let new_list = new_val.map(val => val.name)
+    let old_list = old_val.map(val => val.name)
+    if (new_list.length > old_list.length) {  // add a new attr, calculate scaled value
+      for (let i=0; i<new_list.length; i++) {
+        if (old_list.indexOf(new_list[i]) != i) {
+          if (scale_list.has(new_list[i]))  {} // already have scale, don't calculate again
+          else {
+            CalculateScale(new_list[i])  // todo: need user interaction
+            CalculateScaledRecords(new_list[i])
+          }
+          break
+        }
+      }
+    }
+    else {
+      // delete an attr but try to keep the calculated values, so do nothing
+    }
+  }
+);
+
+const scaled_records = []  // the scaled value of each origin record
+
+function CalculateScale(name) {
+  if (nominal_attr_name.indexOf(name) == -1) {  // quantitative 
+    let value_list = props.origin_records.map(record => record[name])
+    let min = Math.min(...value_list)
+    let max = Math.max(...value_list)
+
+    // todo: user choose between negative and positive correlation 
+
+    let scale = d3.scaleLinear().range([0, 1]).domain([min, max])
+    scale_list.set(name, scale)
+  }
+  else {  // todo: nominal 
+
+  }
+}
+
+function CalculateScaledRecords(name) {
+  if (nominal_attr_name.indexOf(name) != -1) return  //todo: nominal
+
+  let value_list = props.origin_records.map(record => record[name])
+  if (scaled_records.length == 0) { // the first time to calculate, create
+    for (let i=0; i<value_list.length; i++) {   
+      let obj = new Object
+      obj[name] = scale_list.get(name)(value_list[i])
+      scaled_records.push(obj)
+    }
+  }
+  else {
+    for (let i=0; i<value_list.length; i++) {
+      scaled_records[i][name] = scale_list.get(name)(value_list[i])
+    }
+  }
+  
+  console.log("scale_records", scaled_records)
+}
+
+function HandleConfirmMapping() {
+  data.mapping_dialog_visible = false
+}
+
 </script>
 
 <style lang="less" scoped>
