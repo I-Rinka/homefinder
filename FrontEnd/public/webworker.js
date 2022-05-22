3a382ec48b525303a87ceb69c4d1a740250e07c8
I-Rinka <I_Rinka@outlook.com>
2022-05-22 23:02:21 +0800

    initial multithread

--- FrontEnd/public/webworker.js
+++ FrontEnd/public/webworker.js
@@ -1,6 +1,6 @@
-onmessage = function(e) {
-    console.log('Message received from main script',e);
-    // var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
-    // console.log('Posting message back to main script');
-    // postMessage(workerResult);
-  }
+onmessage = function (e) {
+  let scores = e.data.scores;
+
+  scores.sort((a, b) => a.score - b.score);
+  postMessage({ scores: scores });
+};
